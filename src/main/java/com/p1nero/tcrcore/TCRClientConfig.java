d9f5fda55b1fff7712eb62a7e23d338c4cd92c89
P1nero <1070402601@qq.com>
2026-02-19 00:05:09 +0800

    更新UI mod

--- src/main/java/com/p1nero/tcrcore/TCRClientConfig.java
+++ src/main/java/com/p1nero/tcrcore/TCRClientConfig.java
@@ -1,9 +1,7 @@
 package com.p1nero.tcrcore;
 
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = TCRCoreMod.MOD_ID)
 public class TCRClientConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec.IntValue TASK_UI_X = BUILDER.comment("Task UI X").defineInRange("task_ui_x_offset", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
